a62ebb416874f0261c4dd9dea6f4c82e64f5f882
krpcn1r <zaystep09@gmail.com>
2025-12-18 12:31:42 +0300

    Update image_handler.py

--- ImagesIpo/image_handler.py
+++ ImagesIpo/image_handler.py
@@ -5,26 +5,41 @@ class ImageHandler:
         self.path = path
         self.img = None
 
+    # Сохранение изображения по указанному пути
     def save(self):
         if self.img and self.path:
             self.img.save(self.path)
 
+    # Загрузка изображения
     def load(self):
-        self.img = Image.open(self.path)
-        self.img.load()
+        if self.path:
+            self.img = Image.open(self.path)
+            self.img.load()
 
+    # Обрезка центральной части изображения 
     def change_size(self):
+        if not self.img:
+            return
+            
         width, height = self.img.size
+        # Расчет координат для центральной обрезки
         left = (width - 150) // 2
         top = (height - 150) // 2
         right = left + 150
         bottom = top + 150
+        
+        # Выполнение операции crop и сохранение
         self.img = self.img.crop((left, top, right, bottom))
         self.save()
 
+    # Поворот изображения на 90 градусов 
     def rotate_image(self):
+        if not self.img:
+            return
+            
         self.img = self.img.rotate(90, expand=True)
         self.save()
 
+    # Возврат текущего объекта изображения 
     def get_image(self):
         return self.img
