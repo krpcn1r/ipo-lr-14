e262c8a34ffad886ec6417e0364343f6d7423e0e
krpcn1r <zaystep09@gmail.com>
2025-12-18 12:33:22 +0300

    Update main.py

--- ImagesIpo/main.py
+++ ImagesIpo/main.py
@@ -1,5 +1,10 @@
 from image_processor import ImageProcessor
 
+# Запрос пути к файлу у пользователя
 path = input("Введите путь к изображению: ")
+
+# Инициализация процессора изображений
 processor = ImageProcessor(path)
+
+# Применение черно-белого фильтра
 processor.bw_filter()
